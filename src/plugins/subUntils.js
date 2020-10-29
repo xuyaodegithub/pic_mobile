476f4561502d51f2477217827c509679bc4f36a7
Yan <xuyao@17huo.com>
2020-10-29 16:59:08 +0800

    加注释

--- src/plugins/subUntils.js
+++ src/plugins/subUntils.js
@@ -8,12 +8,12 @@ Vue.prototype.$wechat=wechat
 Object.keys(filters).map((val,index)=>{
     Vue.filter(val,filters[val])
 })
-if(BrowserInfo.isWeixin){
+if(BrowserInfo.isWeixin){//微信打开需要重定向  是为了掉jsdk支付
     if(!Cookies.get('token')){
         window.location.href=`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx695bf5a85133ef4c&redirect_uri=http://m.picup.shop/weixinMp/oauth2Callback&state=${encodeURIComponent(window.location.href)}&response_type=code&scope=snsapi_base`
     }
 }
-Vue.prototype.addUrlQuery=(url)=>{
+Vue.prototype.addUrlQuery=(url)=>{//图片链接后加个随机数  base64格式图片原格式返回
     var reg = /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\/?%\s]*?)\s*$/i;
     if(reg.test(url))return url;
     else return url+ (url.indexOf('?')>-1 ? '&' : '?') +'id='+Math.random();
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -20,7 +20,7 @@ export function getToken() {
 
 export function setToken(token) {
   // var inFifteenMinutes = new Date(new Date().getTime() + 15 * 60 * 1000);//设置小于一天的时间
-  Cookies.set(TokenKey, token,{ domain: '.picup.shop' });
+  Cookies.set(TokenKey, token,{ domain: '.picup.shop' });//存在picup.shop域名下
   // let str=JSON.stringify(token)
   // return localStorage.setItem(TokenKey, token)//存储是按照字符串存着，取出来后需要JSON.parse
 }
